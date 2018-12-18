dadce870ecd532e855ca83d44000c50aa52767e5
Ivan Terekh <ivan.terekh@gmail.com>
2018-12-18 07:27:41 +0300

    Updates

---
+++ precache-manifest.8c82e7144791ffeb609136118d1af342.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a792770b6ea4478cc049",
+    "url": "/test-task/static/js/runtime~main.a792770b.js"
+  },
+  {
+    "revision": "8a73c91b0c28bf5fb04c",
+    "url": "/test-task/static/js/main.8a73c91b.chunk.js"
+  },
+  {
+    "revision": "b5ff19297bbff387308d",
+    "url": "/test-task/static/js/1.b5ff1929.chunk.js"
+  },
+  {
+    "revision": "8a73c91b0c28bf5fb04c",
+    "url": "/test-task/static/css/main.63eb33ed.chunk.css"
+  },
+  {
+    "revision": "b5ff19297bbff387308d",
+    "url": "/test-task/static/css/1.15023630.chunk.css"
+  },
+  {
+    "revision": "083d39af3bf0effc70eb3d751af4b5cc",
+    "url": "/test-task/index.html"
+  }
+];
